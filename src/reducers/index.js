23709460e037f423c73d6172c1dfa64171792bb8
Hunter H <h.herrera.academics@gmail.com>
2020-04-24 13:43:43 -0700

    start posts reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+
+//  dummy reducer
+// export default combineReducers({
+//   replaceMe: () => "dummy reducer",
+// });
 
 export default combineReducers({
-  replaceMe: () => "dummy reducer",
+  posts: postsReducer,
 });
