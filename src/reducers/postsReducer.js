df88bb01eed06693392a89c5c6d3842f56f80182
Hunter H <h.herrera.academics@gmail.com>
2020-05-18 23:22:09 -0700

    posts reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-  return 123;
+export default (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      console.log(action.payload);
+      return action.payload;
+    default:
+      return state;
+  }
 };
